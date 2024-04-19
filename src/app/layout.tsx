abb2e4eff08167dcae0b5d076f6abc30adeaf06c
qwerty-fish <qwertyfish>
2024-04-19 19:04:54 +0300

    refactor(app): move to grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,8 +39,10 @@ export default function RootLayout({
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
         <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
-          <TopNav />
-          {children}
+          <div className="h-screen grid grid-rows-[auto,1fr]">
+            <TopNav />
+            <main className="overflow-y-scroll">{children}</main>
+          </div>
           {modal}
           <div id="modal-root" />
         </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export const dynamic = "force-dynamic"
 
 async function Images() {
   const images = await getMyImages();
-  return (<div className="flex flex-wrap justify-center gap-4">
+  return (<div className="flex flex-wrap justify-center gap-4 p-4">
     {images.map((image) => (
       <div key={image.id} className="flex flex-col w-48 h-48">
         <Link href={`/img/${image.id}`} >
